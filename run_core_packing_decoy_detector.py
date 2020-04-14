130fd27a0c246fb4e45f8efe752806c2a68eac90
agrigas115 <47821846+agrigas115@users.noreply.github.com>
2020-04-14 15:10:02 -0400

    Add files via upload

--- run_core_packing_decoy_detector.py
+++ run_core_packing_decoy_detector.py
@@ -13,6 +13,8 @@ parser = argparse.ArgumentParser(description='Core Packing Decoy Detector')
 parser.add_argument('-i', '--input', help='input pdb_file', required=True)  
 args = parser.parse_args()
 
+matlab_root = '/Applications/MATLAB_R2019a.app/bin/matlab'
+
 pdb_file = args.input
 
 print('Beginning scoring on...'+pdb_file)
@@ -24,7 +26,25 @@ with open('./preprocessing_code/pdb_list.txt', 'w') as f:
 
 
 stream = os.popen('pwd')
-pwd = stream.read()
+path = stream.read()
+path = path[:-1]
 
-os.system('python ./preprocessing_code/download_preprocess_pdb.py ./ ./preprocessing_code/pdb_list.txt 1 '+pwd+'/preprocessing_code/')
+preprocess_step1 = 'python ./preprocessing_code/download_preprocess_pdb.py '+path+'/ ./preprocessing_code/pdb_list.txt 1 '+path+'/preprocessing_code/'
+os.system(preprocess_step1)
+os.chdir('./preprocessing_code')
+preprocess_step2 = 'bash  ./bash_edge_code_script_local.sh pdb_list.txt '+path+'/ 1 ./vol_code ./vol_code'
+os.system(preprocess_step2)
+print('\t Calculating rSASA...')
+os.chdir('../rSASA_code/')
+rsasa_command = matlab_root+' -nosplash -nodisplay -nojvm -r "database_sasa"'
+os.system(rsasa_command)
 
+print('\t Calculating Voronoi Volumes...')
+
+print('\t Calculating Residue Volumes...')
+
+print('\t Calculating D_kl...')
+
+print('\t Running SNN...')
+
+print('\t Cleaning up...')
